9fc940a99d5b24097030d028d2f8cf4680110a64
accaria <anugerahbarraqi1997@gmail.com>
2025-06-22 01:39:30 +0700

    feat: landingPage, dropdown, searchbar, temp eventList

--- src/view/landingPage/components/dropdown.tsx
+++ src/view/landingPage/components/dropdown.tsx
@@ -40,7 +40,7 @@ export default function Dropdown(){
             </li>
             <li>
               <Link href="/landingPage" className="block px-4 py-2 hover:bg-gray-100">
-                Contact
+                Contacts
               </Link>
             </li>
           </ul>
